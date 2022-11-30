e441af3a7cb0124cdeecec96334e9dc66e55f48f
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2022-11-30 02:25:44 -0500

    implement verification for account creation

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -1,6 +1,27 @@
-import React, { useRef } from 'react'
+import React, {useRef} from 'react'
 
 export default function SignUp() {
+  const email = useRef('');
+  const username = useRef('');
+  const password = useRef('');
+  const confirmPassword = useRef('');
+
+  function createUser() {
+
+    if(password.current.value == '') {alert("Password is blank"); return;}
+    if(password.current.value != confirmPassword.current.value) {alert("passwords don't match"); return;}
+    if(username.current.value == '') {alert("username is blank"); return;}
+    if(email.current.value == '') {alert("Email is blank"); return;}
+
+    //check if there is an existing user
+    if(true) {
+      //add user
+      console.log(window.location.host);
+    }
+
+    
+  }
+
   return (
   <div id="Authentication"> 
         <button id="signUpWithGoogle">Continue With Google</button>
@@ -9,22 +30,22 @@ export default function SignUp() {
     <div className="AuthenticationInput">
       <div>
         <p>Email address</p>
-        <input type="text" placeholder="email" id="signUpEmail"/>
+        <input type="text" placeholder="email" id="signUpEmail" ref={email}/>
       </div>
       <div>
         <p>User Name</p>
-        <input type="text" placeholder="username " id="signUpUserName"/>
+        <input type="text" placeholder="username " id="signUpUserName" ref={username}/>
       </div>
       <div>
         <p>Password</p>
-        <input type="password" placeholder="password" id="signUpPassword"/>
+        <input type="password" placeholder="password" id="signUpPassword" ref={password}/>
       </div>
       <div>
         <p>Password Confirmation</p>
-        <input type="password" placeholder="confirm password" id="signUpPasswordConfirmation"/>
+        <input type="password" placeholder="confirm password" id="signUpPasswordConfirmation" ref={confirmPassword}/>
       </div>
     </div>
     
-    <button id="login">Sign Up</button>
+    <button id="login" onClick={createUser}>Sign Up</button>
   </div>);
 }
