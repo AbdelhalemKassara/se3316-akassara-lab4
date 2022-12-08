491f239a08a131de79891dc1a7d404eef779bd6c
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2022-12-07 19:48:12 -0500

    remove genres from nav bar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -10,7 +10,6 @@ export default function NavBar(props) {
     <div className="Nav-Bar">
       <p>Abdelhalem</p>
       <Link to="/" className="Nav-Bar-Item" id="Home">Home</Link>
-      <Link to="/Genres" className="Nav-Bar-Item" id="Genres">Genres</Link>
       <Link to="/Search" className="Nav-Bar-Item" id="Search">Search</Link>
       <UserWindow userName={props.userName} onLogout={props.onLogout} isAdmin={props.isAdmin}/>
     </div>
